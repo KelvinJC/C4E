d7898ec63f3512bf7610684870d8a231e10a7251
Kelvin <kelvin.johnson-chu@abujaelectricity.com>
2024-02-22 06:07:50 +0100

    refactor: Change variable names

--- C4Everybody/k&r_chapter3/loop_for_statement/revised_reverse_string.c
+++ C4Everybody/k&r_chapter3/loop_for_statement/revised_reverse_string.c
@@ -10,17 +10,17 @@ int main() {
 
 
 void reverse(string) char string[]; {
-    int i, j, c, p, len;
+    int left_index, right_index, temp, len;
 
     // Calculate string length
     for (len = 0; string[len] != '\0'; len++)
         ;
-        
+
     // Reverse string
-    for (i = 0, j = (len - 1); i < j; i++, j--) {
-        c = string[i];
-        string[i] = string[j];
-        string[j] = c;
+    for (left_index = 0, right_index = (len - 1); left_index < right_index; left_index++, right_index--) {
+        temp = string[left_index];
+        string[left_index] = string[right_index];
+        string[right_index] = temp;
     }
     printf("Reverse of string: %s\n", string);
 }
