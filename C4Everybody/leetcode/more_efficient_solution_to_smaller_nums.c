52302558218726a04626a4ecbce2819561888705
Kelvin <kelvin.johnson-chu@abujaelectricity.com>
2024-03-04 12:15:39 +0100

    refactor: Rename function and variable names to conform  with snake case

--- C4Everybody/leetcode/more_efficient_solution_to_smaller_nums.c
+++ C4Everybody/leetcode/more_efficient_solution_to_smaller_nums.c
@@ -24,7 +24,7 @@ which is more efficient than the naive approach.
 
 This code first creates a binary indexed tree. 
 Then, for each number from right to left, it calculates the sum of all previous numbers 
-which are less than the current number using the getSum() function, 
+which are less than the current number using the get_sum() function, 
 and then increments the count of the current number in the BIT using the update() function. 
 The result is stored in the counts array which is then returned. 
 The OFFSET is used to handle negative numbers.
@@ -61,19 +61,19 @@ void update(BIT *bit, int i) {
         bit->tree[i]++;
 }
 
-int getSum(BIT *bit, int i) {
+int get_sum(BIT *bit, int i) {
     int sum = 0;
     for (; i > 0; i -= (i & -i))
         sum += bit->tree[i];
     return sum;
 }
 
-int* countSmaller(int* nums, int numsSize, int* returnSize) {
-    *returnSize = numsSize;
-    int *counts = (int *)malloc(numsSize * sizeof(int));
+int* count_smaller(int* nums, int nums_size, int* return_size) {
+    *return_size = nums_size;
+    int *counts = (int *)malloc(nums_size * sizeof(int));
     BIT *bit = createBIT();
-    for (int i = numsSize - 1; i >= 0; i--) {
-        counts[i] = getSum(bit, nums[i] + OFFSET);
+    for (int i = nums_size - 1; i >= 0; i--) {
+        counts[i] = get_sum(bit, nums[i] + OFFSET);
         update(bit, nums[i] + OFFSET + 1);
     }
     free(bit);
