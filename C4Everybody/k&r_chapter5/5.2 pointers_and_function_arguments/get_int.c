683684973d8ac73e42907d4f97edf469d526f658
Kelvin <kelvin.johnson-chu@abujaelectricity.com>
2024-03-04 09:54:28 +0100

    Add example on getting integers from input regardless of sign

--- C4Everybody/k&r_chapter5/5.2 pointers_and_function_arguments/get_int.c
+++ C4Everybody/k&r_chapter5/5.2 pointers_and_function_arguments/get_int.c
@@ -1,1 +1,54 @@
 #include <stdio.h>
+#define BUFSIZE 100
+
+char buf[BUFSIZE]; /* buffer for ungetch */
+int bufp = 0; /* next free position in buf */
+
+
+void main() {
+    int get_int();
+    int digit;
+    
+    get_int(&digit);
+    printf("Number is: %d \n", digit);
+}
+
+int get_int(int *pn) {  /* get next integer from input */
+    
+    int c, sign;
+    char getch();
+    void ungetch();
+
+
+    while ((c = getch()) == ' ' || c == '\n' || c == '\t')
+    ; /* skip white space */
+    
+    sign = 1;
+    if (c == '+' || c == '-') { /* record sign */
+        sign = (c == '+') ? 1 : -1;
+        c = getch();
+    }
+
+    for (*pn = 0; c >= '0' && c <= '9'; c = getch()) {
+        *pn = 10 * *pn + c - '0';
+    }
+
+    *pn *= sign;
+    if (c != EOF) {
+        ungetch(c);
+    }
+    return(c);
+}
+
+
+char getch() /* get a (possibly pushed back) character */ {
+  return((bufp > 0) ? buf[--bufp] : getchar());
+}
+
+void ungetch (int c) /* push character back on input */ {
+  if (bufp > BUFSIZE)
+    printf("ungetch: too many characters\n");
+  else
+    buf[bufp++] = c;
+}
+
