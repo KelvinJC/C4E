2fb09443991f6fb8903398da6952474998ee679f
Kelvin <kelvin.johnson-chu@abujaelectricity.com>
2024-03-07 19:12:14 +0100

    Edit intro

--- C4Everybody/k&r_chapter5/exercises_on_pointers/tindex.c
+++ C4Everybody/k&r_chapter5/exercises_on_pointers/tindex.c
@@ -1,6 +1,5 @@
 /*
-Exercise 4-1. 
-Write the function t_index(s, t), 
+Rewrite the function t_index(s, t), 
 which returns the position of the rightmost occurrence of t in s, 
 or -1 if there is none.
 */
